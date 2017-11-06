8e71c67e6620dceea290052c50fa82b1efcc095e
LeMona Wyatt <lemonawyatt@gmail.com>
2017-11-06 16:25:33 +0000

    added pwd and date to bash.js

---
+++ bash.js
@@ -0,0 +1,19 @@
+process.stdout.write('prompt > ');
+
+//The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+
+   if (cmd === 'pwd') {
+    var pwd = process.argv0;
+    process.stdout.write(pwd);
+   }
+
+   if (cmd === 'date') {
+    var date = Date();
+    process.stdout.write(date);
+   }
+  
+  process.stdout.write('\nprompt > ');
+
+});
